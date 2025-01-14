81c2ebc8a5c6d772cb5fba97f117d5383bf31e26
saikenat63 <saikenat63@gmail.com>
2025-01-14 18:52:41 +0530

    add 1th model

--- a.py
+++
@@ -1,8 +0,0 @@
-import tensorflow as tf
-
-# List available physical GPUs
-physical_devices = tf.config.list_physical_devices('GPU')
-if physical_devices:
-    print(f"GPU is available: {physical_devices}")
-else:
-    print("GPU is not available")
--- gitpush.py
+++ gitpush.py
@@ -2,18 +2,40 @@ import os
 import subprocess as sp
 import time
 
+file = "/home/c3ilab/Documents/lebc/super_resolution_model.weights.keras"
+i = 1
 while True:
+    if os.path.isfile(file):
 
-    cmd = [
-        "git",
-        "status"
-    ]
+        cmd = [
+            "mv", file, f"{file}_{i}"
+        ]
+        res = sp.run(cmd)
+        print(res.stdout)
+        print(res.stderr)
+        
 
-
-
-
-
-
-
-
+        cmd = [
+            "git", "add", ".",
+        ]
+        res = sp.run(cmd)
+        print(res.stdout)
+        print(res.stderr)
+        
+        cmd = [
+            "git", "commit", "-m", f"add {i}th model",
+        ]
+        res = sp.run(cmd)
+        print(res.stdout)
+        print(res.stderr)
+        
+        cmd = [
+            "git", "push",
+        ]
+        res = sp.run(cmd)
+        print(res.stdout)
+        print(res.stderr)
+        
+        i += 1
+        
     time.sleep(10)
